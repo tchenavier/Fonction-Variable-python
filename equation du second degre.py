4b3821cb40af51cc1a0ffe781ce8f740d42c0803
tchenavier <thibaud.chenavier@gmail.com>
2025-10-13 14:43:10 +0200

    ajout d'un verificateur pour la valeur de a

--- equation du second degre.py
+++ equation du second degre.py
@@ -5,18 +5,19 @@ b = float(input ("La valeur de b"))
 c = float(input ("La valeur de c"))
 
 d=b**2-4*a*c
-
-if (d==0):   #condition pour si d=0 donc une seul racine
-    X=-b/(2*a)
-    print("Une seule racinne réelle qui est",X)
-    #valeur teste a=3 b=-12 c=12 x=2
-elif (d>0):  #https://openclassrooms.com/fr/courses/7168871-apprenez-les-bases-du-langage-python/7168878-controlez-le-deroulement-de-votre-programme-avec-des-conditions
-    r= sqrt(d) #racine carré
-    X1=(-b-r)/(2*a)
-    X2=(-b+r)/(2*a)
-
-    print("La premiere racine",X1,"la deuxieme racine",X2) #affiche les deux racine
-    #valeur teste a=2; b=4;c=-6 x=-3 x2 = 1
+if (a==0):
+    if (d==0):   #condition pour si d=0 donc une seul racine
+        X=-b/(2*a)
+        print("Une seule racinne réelle qui est",X)
+        #valeur teste a=3 b=-12 c=12 x=2
+    elif (d>0):  #https://openclassrooms.com/fr/courses/7168871-apprenez-les-bases-du-langage-python/7168878-controlez-le-deroulement-de-votre-programme-avec-des-conditions
+        r= sqrt(d) #racine carré
+        X1=(-b-r)/(2*a)
+        X2=(-b+r)/(2*a)
+        print("La premiere racine",X1,"la deuxieme racine",X2) #affiche les deux racine
+        #valeur teste a=2; b=4;c=-6 x=-3 x2=1
+    else :
+        print("Pas de racine réelle")
+        # valeur teste a=3 b=-2 c=1
 else :
-    print("Pas de racine réelle")
-    # valeur teste a=3 b=-2 c=1
+    print("La valeur de a n'est pas valable")
