ea5ec55698fa0f2e65f32006c67e0f3b65bf09fa
tchenavier <thibaud.chenavier@gmail.com>
2025-11-17 14:35:48 +0100

    exercise 4

--- MatplotlibActiviter.py
+++ MatplotlibActiviter.py
@@ -1,7 +1,7 @@
 from pylab import *
 xlim(0,10)
 ylim(0,10)
-x=[2,5,8,2]#le dernier x2;y2 c'est pour le trai de retour
-y=[2,8,2,2]
+x=[3,3,7,7,3]#le dernier x2;y2 c'est pour le trai de retour
+y=[3,7,7,3,3]
 plot(x,y,'-*r')
 show()
---
+++ MatplotlibActiviterPoind.py
@@ -0,0 +1,16 @@
+from pylab import *
+xlim(0,20)
+ylim(0,10)
+
+for i in range (1,11):
+    x=5+i
+    y= 5
+    i+1
+    plot(x,y,'*r')
+
+for i in range (1,11):
+    x=5+i
+    y= 3
+    i+1
+    plot(x,y,'*r')
+show()
