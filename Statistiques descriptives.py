6fc870a4b94dd5b4058001193c8631ff86f86c1c
tchenavier <thibaud.chenavier@gmail.com>
2025-11-24 14:48:17 +0100

    commentaire

--- Statistiques descriptives.py
+++ Statistiques descriptives.py
@@ -1,5 +1,5 @@
-from math import *
-from pylab import *
+from math import *  # inutile pour le moment
+from pylab import * # inutile pour le moment
 
 Total = 0
 Moyenne = 0.00
@@ -8,6 +8,7 @@ maximum = 552.129
 for i in range (1,31):
     e = float(input ("Temprerature\n"))
     Total = Total + e
+    #partie pour les minimum et le maximum
     if ( e< minimum or minimum == 552.129):
         minimum = e
     if ( e > maximum or maximum == 552.129):
