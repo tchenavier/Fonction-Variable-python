ad5a341b208229509fadf9637ec16dac0ae630b9
tchenavier <thibaud.chenavier@gmail.com>
2025-11-26 16:13:33 +0100

    ex 7

--- Statistiques descriptives.py
+++ Statistiques descriptives.py
@@ -5,6 +5,9 @@ Total = 0
 Moyenne = 0.00
 minimum = 552.129
 maximum = 552.129
+aout = [22, 22.6, 21.1, 21.9, 18.3, 18.7, 21.7, 21.1, 23.6, 27.4, 25.1, 24.9, 23.7, 28.9,  23.2, 26.1, 26.1, 29.8, 28.2, 28.7, 25.9, 26.7, 30.3, 25.2, 21.8, 22, 20.9, 20.7, 20.6, 19.5, 18.8]
+sort(aout) #Permettant de ranger les valeurs du tableau dans l'ordre  croissant
+
 for i in range (1,31):
     e = float(input ("Temprerature\n"))
     Total = Total + e
@@ -13,6 +16,8 @@ for i in range (1,31):
         minimum = e
     if ( e > maximum or maximum == 552.129):
         maximum = e
-        
+Etendue = maximum-minimum
+
 Moyenne = Total/31
+
 print(Moyenne)
