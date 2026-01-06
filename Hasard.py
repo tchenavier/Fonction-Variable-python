ceeec9428f98a569454a60ebfaf2b947f8abe8d8
tchenavier <thibaud.chenavier@gmail.com>
2026-01-06 11:43:07 +0100

    exercise 2 Hasard

---
+++ Hasard.py
@@ -0,0 +1,6 @@
+from random import *
+
+for i in range (1 , 3):
+   y = random(1,6)
+   x = x+y
+print (x)
--- Statistiques descriptives.py
+++ Statistiques descriptives.py
@@ -7,8 +7,8 @@ minimum = 552.129
 maximum = 552.129
 aout = [22, 22.6, 21.1, 21.9, 18.3, 18.7, 21.7, 21.1, 23.6, 27.4, 25.1, 24.9, 23.7, 28.9,  23.2, 26.1, 26.1, 29.8, 28.2, 28.7, 25.9, 26.7, 30.3, 25.2, 21.8, 22, 20.9, 20.7, 20.6, 19.5, 18.8]
 sort(aout) #Permettant de ranger les valeurs du tableau dans l'ordre  croissant
-
-for i in range (1,31):
+taille = len(aout) # récuper la taille du tableau 
+for i in range (0,taille):
     e = float(input ("Temprerature\n"))
     Total = Total + e
     #partie pour les minimum et le maximum
